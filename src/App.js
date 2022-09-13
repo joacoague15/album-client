90d10888004862e419d01fd6e9a33cf89013d065
Mauricio Hunau <maurih@live.com>
2022-09-12 21:59:56 -0300

    maquetado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom";
+
+import Bar from './components/Bar';
+import Album from './components/Album';
+import Packages from './components/Packages';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Bar />
+      <Routes>
+        <Route path='/' element={<Album/>} />
+        <Route path='/packages' element={<Packages/>}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Packages.js
@@ -0,0 +1,12 @@
+import { Paper } from '@mui/material'
+import React from 'react'
+
+function Packages() {
+  return (
+    <Paper sx={{position:"absolute",margin:"0px auto", width:"200px", height:"260px"}}>
+        
+    </Paper>
+  )
+}
+
+export default Packages
