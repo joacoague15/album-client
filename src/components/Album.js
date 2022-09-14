efd15ad2592dba91f99708e0c267a68adedc4aa4
joacoague15 <joacoague15@gmail.com>
2022-09-14 20:54:52 +0100

    Changes on component structure

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,20 +1,23 @@
-import { Box } from '@mui/material'
+import {Box} from '@mui/material'
 import React from 'react'
-import Figuritas from '../subComponents/Figuritas'
-import { Link } from 'react-router-dom';
+import Sticker from './Sticker';
+import {useNavigate} from "react-router-dom";
 
-const figuritas = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
 
 function Album() {
-    return (<>
+    const navigate = useNavigate();
+
+
+    const redirectToPackages = () => {
+        navigate('/packages');
+    }
+
+    return (
         <Box sx={{ position: "absolute", top: 120, bottom: "auto", width:"auto", bgcolor: "#010747", height:"auto"}}>
-            <Box sx={{ display: "flex", flexWrap: "wrap", gap: "2rem", justifyContent: "center", padding:"30px 100px" }}>
-                {figuritas.map((index) => {
-                    return <><Link to="/packages"><Figuritas key={index} /></Link></>
-                })}
-            </Box>
+                <Sticker onClick={redirectToPackages} />
+                <Sticker onClick={redirectToPackages} />
+                <Sticker onClick={redirectToPackages} />
         </Box>
-    </>
     )
 }
 
